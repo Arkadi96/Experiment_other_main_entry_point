d56f1509338aacfc7030a7b41fbf3f8f4479540a
Arkadi96 <69543920+Arkadi96@users.noreply.github.com>
2020-08-13 02:07:47 +0400

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World (Main)");
+        }
+
+        //desired entry point
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Hello World (Desired Main)");
+        }
+    }
+
+}
